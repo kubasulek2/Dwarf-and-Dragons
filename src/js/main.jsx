daa7c620f325b055eb5603ad3516bb9e5ae8091f
Kuba Sułkowski <kubasulek2@gmail.com>
2018-02-21 21:59:57 +0100

    Add: create monters on board, monsters wont appear in the same location

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { connect, Provider } from 'react-redux';
 require('../sass/main.scss');
-    const image = require('../img/sprite2.png');
-
+import imgs from './images'
+console.log(imgs);
 class Game extends React.Component {
     constructor(props){
         super(props)
+        this.state ={
+            monsters:[],
+            hero:{},
+            level: 1
         }
+    }
+
 
     render(){
         return(
@@ -25,6 +31,49 @@ class Game extends React.Component {
         )
     }
 
+
+    drawMonsters =  ()=> {
+        let ctx = this.canvas.getContext('2d');
+        let monsterImg = new Image();
+        monsterImg.src = imgs[1];
+
+        let monstersAmount = Math.ceil(Math.random()*3)+Math.floor(this.state.level);
+
+        let monstersArr =[];
+
+        monsterImg.onload= () =>{
+            for(let i = 0; i < monstersAmount; i++){
+                let x = Math.round(Math.random()*(92-10)+10)*10;
+                let y = Math.round(Math.random()*(62-10)+10)*10;
+
+                if(i > 0){
+
+                    if(x !== monstersArr[i-1].positionX && y !== monstersArr[i-1].positionY){
+
+                        ctx.drawImage(monsterImg,x,y,70,70);
+                        monstersArr.push({positionX: x, positionY:y});
+
+                    }
+                }else{
+                    ctx.drawImage(monsterImg,x,y,70,70);
+                    monstersArr.push({positionX: x, positionY:y});
+                }
+
+
+
+            }
+        };
+        this.setState({})
+
+    };
+
+
+
+
+
+
+
+
     componentDidMount(){
 
         const ctx = this.canvas.getContext('2d');
@@ -34,25 +83,8 @@ class Game extends React.Component {
         let detectionLeft = false;
         let detectionUp = false;
         let detectionDown = false;
-        let img = new Image();
-        img.src = image;
-
-
-        let monster={
-            x: 0,
-            y: 0,
-            randomize: function () {
-
-            },
-            draw: function () {
-                this.x = Math.round(Math.random()*(92-10)+10)*10
-                this.y = Math.round(Math.random()*(62-10)+10)*10
-                console.log(this.x, this.y);
-
-              ctx.fillRect(this.x,this.y,80,80)
-            }
-        }
-
+        let heroImg = new Image();
+        heroImg.src = imgs[3];
 
         let hero = {
 
@@ -63,9 +95,9 @@ class Game extends React.Component {
             imSize: 80,
             draw: function() {
 
-                console.log(this.cX, detectionLeft);
                 this.checkDetection();
-                ctx.drawImage(img, this.imX, this.imY, this.imSize, this.imSize, this.cX, this.cY, this.imSize,this.imSize);
+                ctx.drawImage(heroImg, this.imX, this.imY, this.imSize, this.imSize, this.cX, this.cY, this.imSize,this.imSize);
+
 
                 },
 
@@ -88,30 +120,33 @@ class Game extends React.Component {
                 }
 
         };
-        img.onload = function(){
+        heroImg.onload=()=>{
+            hero.draw();}
 
-            hero.draw();
-            monster.randomize()
-            monster.draw()
+        this.drawMonsters()
 
-            window.addEventListener("keydown", (e)=> {
-                if(e.keyCode === 40&&!detectionDown){
-                    //w dol
-                    hero.move(160,0,1)
-                }else if(e.keyCode === 38&&!detectionUp){
-                    // w gore
-                    hero.move(0,0,-1)
-                }else if(e.keyCode === 39 &&!detectionRight){
-                    //w prawo
-                    hero.move(80,1,0)
-                }else if(e.keyCode === 37 &&!detectionLeft){
-                    //w lewo
-                    hero.move(240,-1,0)
-                }
-            })
 
-        };
-      }
+
+
+
+        window.addEventListener("keydown", (e)=> {
+            if(e.keyCode === 40&&!detectionDown){
+                //w dol
+                hero.move(160,0,1)
+            }else if(e.keyCode === 38&&!detectionUp){
+                // w gore
+                hero.move(0,0,-1)
+            }else if(e.keyCode === 39 &&!detectionRight){
+                //w prawo
+                hero.move(80,1,0)
+            }else if(e.keyCode === 37 &&!detectionLeft){
+                //w lewo
+                hero.move(240,-1,0)
+            }
+        })
+
+    }
+
 };
 
 
