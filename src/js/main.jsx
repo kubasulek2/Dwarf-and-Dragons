0837137bd35ebd8fa4cbb3f95b742d1527e4aee7
Kuba Sułkowski <kubasulek2@gmail.com>
2018-02-22 15:07:29 +0100

    Add: canvas background, rewrite all functions from object functions to component function. Disappearing Hero bug fixed

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -10,10 +10,22 @@ class Game extends React.Component {
         super(props)
         this.state ={
             monsters:[],
-            hero:{},
+            heroLevel: 1,
+            cX: 0,
+            cY: 250,
+            imX: 0,
+            imY: 0,
+            imSize: 80,
+            detectionRight: false,
+            detectionLeft: false,
+            detectionUp: false,
+            detectionDown: false,
             level: 1
+
+
         }
     }
+
 
 
     render(){
@@ -22,7 +34,7 @@ class Game extends React.Component {
                 <div className="header"></div>
 
                 <div className="wrapper">
-                    <canvas width="1000" height="700" id="myCanvas" ref={(canv) => { this.canvas = canv; }}></canvas> {/*mam this.canvas*/}
+                    <canvas className="canva" width="1000" height="700" id="myCanvas" ref={(canv) => { this.canvas = canv; }}></canvas> {/*mam this.canvas*/}
                     <div className="sidebar">
                         <h1>Name: Lothar</h1>
                     </div>
@@ -44,7 +56,7 @@ class Game extends React.Component {
         monsterImg.onload= () =>{
             for(let i = 0; i < monstersAmount; i++){
                 let x = Math.round(Math.random()*(92-10)+10)*10;
-                let y = Math.round(Math.random()*(62-10)+10)*10;
+                let y = Math.round(Math.random()*(62-25)+25)*10;
 
                 if(i > 0){
 
@@ -63,86 +75,71 @@ class Game extends React.Component {
 
             }
         };
-        this.setState({})
+        this.setState({monsters: monstersArr})
 
     };
 
 
-
-
-
-
-
-
-    componentDidMount(){
-
+    draw(hero) {
         const ctx = this.canvas.getContext('2d');
-        const canvaWidth = 1000;
-        const canvaHeight = 700;
-        let detectionRight = false;
-        let detectionLeft = false;
-        let detectionUp = false;
-        let detectionDown = false;
         let heroImg = new Image();
         heroImg.src = imgs[3];
 
-        let hero = {
-
-            cX: 0,
-            cY: 0,
-            imX: 0,
-            imY: 0,
-            imSize: 80,
-            draw: function() {
-
-                this.checkDetection();
-                ctx.drawImage(heroImg, this.imX, this.imY, this.imSize, this.imSize, this.cX, this.cY, this.imSize,this.imSize);
+        this.checkDetection();
+        ctx.drawImage(hero, this.state.imX, this.state.imY, this.state.imSize, this.state.imSize, this.state.cX, this.state.cY, this.state.imSize,this.state.imSize);
 
 
-                },
+    }
+    move(imX,cX,cY, hero) {
+        const ctx = this.canvas.getContext('2d');
+        ctx.clearRect(this.state.cX, this.state.cY, this.state.imSize, this.state.imSize,);
 
+        this.setState({
+            cX: this.state.cX += cX * 10,
+            cY: this.state.cY += cY * 10,
+            imX:this.state.imX= imX
+        });
 
-            move: function (imX,cX,cY) {
+        this.draw(hero);
+    }
 
-                    ctx.clearRect(this.cX, this.cY, this.imSize, this.imSize,);
-                    this.cX += cX * 10;
-                    this.cY += cY * 10;
-                    this.imX = imX;
-                    this.draw();
-            },
+    checkDetection() {
+        const canvaWidth = 1000;
+        const canvaHeight = 700;
+        this.setState({
+            detectionRight: this.state.cX > canvaWidth - this.state.imSize-20? true: false,
+            detectionLeft: this.state.cX < 20? true: false,
+            detectionDown: this.state.cY > canvaHeight - this.state.imSize-20? true: false,
+            detectionUp: this.state.cY < 250? true: false,
+        })
+    }
 
-            checkDetection: function () {
+    componentDidMount(){
+        let heroImg = new Image();
+        heroImg.src = imgs[3];
 
-                detectionRight = this.cX > canvaWidth - this.imSize-20? true: false;
-                detectionLeft = this.cX < 20? true: false;
-                detectionDown = this.cY > canvaHeight - this.imSize-20? true: false;
-                detectionUp = this.cY < 20? true: false;
-                }
-
-        };
         heroImg.onload=()=>{
-            hero.draw();}
+            this.draw(heroImg);
+        };
+        this.drawMonsters();
 
-        this.drawMonsters()
+        document.addEventListener("keydown", (e)=> {
 
-
-
-
-
-        window.addEventListener("keydown", (e)=> {
-            if(e.keyCode === 40&&!detectionDown){
+            if(e.keyCode === 40&&!this.state.detectionDown){
                 //w dol
-                hero.move(160,0,1)
-            }else if(e.keyCode === 38&&!detectionUp){
+                this.move(160,0,1,heroImg)
+            }else if(e.keyCode === 38&&!this.state.detectionUp){
                 // w gore
-                hero.move(0,0,-1)
-            }else if(e.keyCode === 39 &&!detectionRight){
+                this.move(0,0,-1,heroImg)
+            }else if(e.keyCode === 39 &&!this.state.detectionRight){
                 //w prawo
-                hero.move(80,1,0)
-            }else if(e.keyCode === 37 &&!detectionLeft){
+                this.move(80,1,0,heroImg)
+            }else if(e.keyCode === 37 &&!this.state.detectionLeft){
                 //w lewo
-                hero.move(240,-1,0)
+                this.move(240,-1,0,heroImg)
             }
+
+
         })
 
     }
